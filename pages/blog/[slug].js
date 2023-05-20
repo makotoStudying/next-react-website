ff0fc3df412d07ffe6e33750ad7f95df64bb7e6a
Makoto <makoto19950928@outlook.jp>
2023-05-20 16:57:43 +0900

    更新

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -43,24 +43,23 @@ export default function Post({
       <article>
         <Postheader title={title} subtitle="Blog Article" publish={publish} />
 
-        <figure>
-          <Image
-            key={eyecatch.url}
-            src={eyecatch.url}
-            alt=""
-            layout="responsive"
-            width={eyecatch.width}
-            height={eyecatch.height}
-            sizes="(min-width: 1152px) 1152px, 100vw"
-            priority
-            placeholder="blur"
-            blurDataURL={eyecatch.blurDataURL}
-          />
-        </figure>
-
         <TwoColumn>
           <TwoColumnMain>
             <PostBody>
+              <figure>
+                <Image
+                  key={eyecatch.url}
+                  src={eyecatch.url}
+                  alt=""
+                  layout="responsive"
+                  width={eyecatch.width}
+                  height={eyecatch.height}
+                  sizes="(min-width: 1152px) 1152px, 100vw"
+                  priority
+                  placeholder="blur"
+                  blurDataURL={eyecatch.blurDataURL}
+                />
+              </figure>
               <ConvertBody contentHTML={content} />
             </PostBody>
           </TwoColumnMain>
