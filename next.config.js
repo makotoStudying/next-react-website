8bb2fa5264dd09ae871f674d951d7851f052a6f6
Makoto <makoto19950928@outlook.jp>
2023-12-27 19:47:39 +0900

    upgrade

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.microcms-assets.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+      },
+    ],
   },
 };
 
